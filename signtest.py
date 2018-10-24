6928413518c39bed437a1048da589e3518232261
Andrew Chiw <randomshinichi4869@gmail.com>
2018-10-24 17:57:29 +0200

    signtest.py has mock data, outputs it in a form suitable for curl

--- signtest.py
+++ signtest.py
@@ -1,23 +1,32 @@
-from eth_utils import decode_hex, keccak
-from server import prefix, convert_eth_signature_and_data
-import binascii
-
 from coincurve import PublicKey, PrivateKey
-signature_jozef_orig = decode_hex(
-    'CC9E2B0B338D1BDD1328D88E6750F7CE5F7C6DEA8D73D4DE43E7755029576C1F07D8954640CB698A802729C4C31B17E9D1BF92B23A6CAEEAA2477087D5CCF3541C')
-message = b'{"timestamp":4180,"temperature":{"val1":25,"val2":173643},"lat":{"val1":1,"val2":2},"lon":{"val1":-4,"val2":5}}'
+from raiden_libs import utils
+import binascii
+import time
+import json
+
+trip_along_ringbahn_raw = [
+    {'timestamp': int(time.time()) - 10000, 'lat': {'val1': 52, 'val2': 536131},
+     'lon': {'val1': 13, 'val2': 447444}, 'temperature': {"val1": 19, "val2": 0}},
+    {'timestamp': int(time.time()) - 9000, 'lat': {'val1': 52, 'val2': 538221},
+     'lon': {'val1': 13, 'val2': 44376}, 'temperature': {"val1": 20, "val2": 0}},
+    {'timestamp': int(time.time()) - 8000, 'lat': {'val1': 52, 'val2': 540247},
+     'lon': {'val1': 13, 'val2': 43899}, 'temperature': {"val1": 21, "val2": 0}},
+    {'timestamp': int(time.time()) - 7000, 'lat': {'val1': 52, 'val2': 541751},
+     'lon': {'val1': 13, 'val2': 43513}, 'temperature': {"val1": 22, "val2": 0}},
+    {'timestamp': int(time.time()) - 6000, 'lat': {'val1': 52, 'val2': 54264},
+     'lon': {'val1': 13, 'val2': 433692}, 'temperature': {"val1": 23, "val2": 0}},
+    {'timestamp': int(time.time()) - 5000, 'lat': {'val1': 52, 'val2': 543007},
+     'lon': {'val1': 13, 'val2': 431339}, 'temperature': {"val1": 24, "val2": 0}},
+    {'timestamp': int(time.time()) - 4000, 'lat': {'val1': 52, 'val2': 543755},
+     'lon': {'val1': 13, 'val2': 428731}, 'temperature': {"val1": 25, "val2": 0}},
+    {'timestamp': int(time.time()) - 3000, 'lat': {'val1': 52, 'val2': 544295},
+     'lon': {'val1': 13, 'val2': 427207}, 'temperature': {"val1": 27, "val2": 0}},
+]
 
 private_key = PrivateKey()
-public_key = private_key.public_key
-private_key_2 = PrivateKey()
-public_key_2 = private_key.public_key
-
-# Prepare data! Message needs to be prefixed. Signature needs to be EIP converted?
-signature_jozef, message = convert_eth_signature_and_data(signature_jozef_orig, message)
-
-signature = private_key.sign_recoverable(prefix(message), hasher=keccak)
-print(signature.hex())
-
-public_key_recovered = PublicKey.from_signature_and_message(signature, prefix(message), hasher=keccak)
-
-# Apparently verify() is never used because it doesn't do the signature schema!
+trip_along_ringbahn = []
+for t in trip_along_ringbahn_raw:
+    t_json = json.dumps(t)
+    signature = utils.sign(private_key, t_json.encode()).hex().upper()
+    trip_along_ringbahn.append((t_json, signature))
+    print('curl http://localhost:5000/data -X POST --header "X-Anyledger-Sig: {}" --header "Content-Type: application/json" -d \'{}\''.format(signature, t_json))
